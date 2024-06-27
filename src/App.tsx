055e7462264fd8943cebf95fc19c932a6fe95f21
wendryuslima <141366401+wendryuslima@users.noreply.github.com>
2024-06-27 19:10:00 -0300

    chore: I changed the index and the app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
 import { FunctionComponent, useState } from 'react'
 import {BrowserRouter, Route, Routes} from 'react-router-dom'
 
-//Page
-import Header from './components/header/header.components'
 import HomePage from './pages/home/home.page'
 
 
